3bbea9c322ddaeae5f884e948dec44720dcf086e
Lacey Umamoto <lcumamoto@gmail.com>
2020-10-24 20:28:29 -0700

    added function hooked up to button, in middle of extracting data from inputs

--- frontend/cavexml-frontend/src/app/cavexml-search/cavexml-search.component.ts
+++ frontend/cavexml-frontend/src/app/cavexml-search/cavexml-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../../../data/data.json';
+
 
 @Component({
   selector: 'app-cavexml-search',
@@ -6,32 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cavexml-search.component.scss']
 })
 export class CavexmlSearchComponent implements OnInit {
+  public tag; 
+  public length;
+  public vertical_extent;
+  public altitude;
+  public length_gtlt;
+  public vertical_extent_gtlt;
+  public altitude_gtlt;
+
+  // when Submit button is pressed
+  onClickMe() {
+    this.tag;
+    this.length;
+    this.vertical_extent;
+    this.altitude;
+    this.length_gtlt;
+    this.vertical_extent_gtlt;
+    this.altitude_gtlt;
+    window.alert("length="+this.length);
+  }
+
+  // when length value is entered
+  OnInputLen(value) {
+    this.length = value;
+  }
+
+  // when vertical_extent value is entered
+  OnInputVE(value) {
+    this.vertical_extent = value;
+  }
+
+  // when altitude value is entered
+  OnInputAlt(value) {
+    this.altitude = value;
+  }
 
   // This is dummy data
   public dummyData = [
     {
-      name: 'Test Cave',
+      name: 'Test Cave 1',
       tags: 'Cave, SSSI',
       length: 112,
       vr: 22,
       altitude: 123
     },
     {
-      name: 'Test Cave',
+      name: 'Test Cave 2',
       tags: 'Cave, SSSI',
       length: 112,
       vr: 22,
       altitude: 123
     },
     {
-      name: 'Test Cave',
+      name: 'Test Cave 3',
       tags: 'Cave, SSSI',
       length: 112,
       vr: 22,
       altitude: 123
     },
-];
-  constructor() { }
+  ];
+
+  constructor() {
+    this.dummyData = data
+    // console.log('Reading local json files');
+    // console.log(data);
+  }
 
   ngOnInit() {
   }
